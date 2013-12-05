deaedb993d1f719a0ab478a2173aaaa50ae772e1
mnascimento <martim00@gmail.com>
2013-12-05 19:36:34 -0200

    organizing packages

---
+++ src/instrumenter/core/SCValue.java
@@ -0,0 +1,37 @@
+package instrumenter.core;
+import java.util.Set;
+
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.analysis.SourceValue;
+
+
+public class SCValue extends SourceValue {
+	
+	
+	private String name = "";
+
+	public SCValue(int size) {
+		super(size);
+	}
+
+	public SCValue(int size, AbstractInsnNode insn, String name) {
+		super(size, insn);
+		this.name = name;
+	}
+
+	public SCValue(int size, Set<AbstractInsnNode> insns, String name) {
+		super(size, insns);
+		this.name = name;
+	}
+	
+	public SCValue(int size, String name) {
+		super(size);
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+	
+
+}
---
+++ src/instrumenter/core/Utils.java
@@ -0,0 +1,20 @@
+package instrumenter.core;
+import org.objectweb.asm.tree.AnnotationNode;
+import org.objectweb.asm.tree.MethodNode;
+
+
+public class Utils {
+
+	public static boolean hasTestAnnotation(MethodNode methodNode) {
+		if (methodNode.visibleAnnotations == null)
+			return false;
+
+		for (AnnotationNode annotation : methodNode.visibleAnnotations) {
+
+			if (annotation.desc.equals("Lorg/junit/Test;"))
+				return true;
+
+		}
+		return false;
+	}
+}
